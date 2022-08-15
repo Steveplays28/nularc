c6d7a5029b83259df687628e7285059fff115d31
Darion Spaargaren <djspaargaren@outlook.com>
2022-08-16 01:23:34 +0200

    Add local IP endpoint, fix edge cases, add packet listener

--- source/Server/Server.cs
+++ source/Server/Server.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
@@ -8,37 +9,41 @@ namespace NExLib.Server
 {
 	public class Server
 	{
-		public const int MaxPacketsReceivedPerTick = 5;
-
-		public UdpClient UdpClient;
-		public Dictionary<IPEndPoint, int> ConnectedClientsIpToId = new Dictionary<IPEndPoint, int>();
-		public Dictionary<int, IPEndPoint> ConnectedClientsIdToIp = new Dictionary<int, IPEndPoint>();
-		public Dictionary<IPEndPoint, int> SavedClientsIpToId = new Dictionary<IPEndPoint, int>();
-		public Dictionary<int, IPEndPoint> SavedClientsIdToIp = new Dictionary<int, IPEndPoint>();
-		public bool HasStarted;
-		public bool IsStopping;
-		public delegate void PacketCallback(Packet packet, IPEndPoint clientIPEndPoint);
-		public event PacketCallback PacketReceived;
+		public int MaxPacketsReceivedPerTick = 5;
+		public delegate void PacketReceivedEventHandler(Packet packet, IPEndPoint IPEndPoint);
+		public event PacketReceivedEventHandler PacketReceived;
+		public event PacketReceivedEventHandler Connected;
+		public event PacketReceivedEventHandler Disconnected;
+		public UdpClient UdpClient { get; private set; }
+		public IPEndPoint IPEndPoint { get; private set; }
+		public bool HasStarted { get; private set; }
+		public bool IsStopping { get; private set; }
+		public Dictionary<IPEndPoint, int> ConnectedClientsIPToID { get; private set; } = new Dictionary<IPEndPoint, int>();
+		public Dictionary<int, IPEndPoint> ConnectedClientsIDToIP { get; private set; } = new Dictionary<int, IPEndPoint>();
+		public Dictionary<IPEndPoint, int> SavedClientsIpToId { get; private set; } = new Dictionary<IPEndPoint, int>();
+		public Dictionary<int, IPEndPoint> SavedClientsIdToIp { get; private set; } = new Dictionary<int, IPEndPoint>();
 		public readonly LogHelper LogHelper = new LogHelper("[NExLib (Server)]: ");
 
-		private readonly IPEndPoint serverEndPoint;
-		private Task ReceivePacketsTask;
+		private readonly Dictionary<int, List<PacketReceivedEventHandler>> PacketListeners = new Dictionary<int, List<PacketReceivedEventHandler>>();
 
-		public Server(int port)
+		public Server()
 		{
-			serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
-			UdpClient = new UdpClient(serverEndPoint);
+			PacketReceived += OnPacketReceived;
+
+			Listen((int)DefaultPacketTypes.Connect, OnConnected);
+			Listen((int)DefaultPacketTypes.Connect, OnDisconnected);
 		}
 
-		public void Start()
+		public void Start(int port)
 		{
-			PacketReceived += PacketReceivedHandler;
+			UdpClient = new UdpClient(port);
+			IPEndPoint = UdpClient.Client.LocalEndPoint as IPEndPoint;
 
 			HasStarted = true;
-			LogHelper.LogMessage(LogHelper.LogLevel.Info, $"Server started on {serverEndPoint}.");
+			LogHelper.LogMessage(LogHelper.LogLevel.Info, $"Server started on {IPEndPoint}.");
 		}
 
-		public async void Stop()
+		public void Stop()
 		{
 			try
 			{
@@ -49,9 +54,10 @@ namespace NExLib.Server
 				}
 
 				IsStopping = true;
-				await Task.WhenAll(ReceivePacketsTask);
-
 				UdpClient.Close();
+				IPEndPoint = null;
+				ConnectedClientsIDToIP = new Dictionary<int, IPEndPoint>();
+				ConnectedClientsIPToID = new Dictionary<IPEndPoint, int>();
 
 				HasStarted = false;
 				IsStopping = false;
@@ -63,10 +69,9 @@ namespace NExLib.Server
 			}
 		}
 
-		public async void Tick()
+		public void Tick()
 		{
-			ReceivePacketsTask = ReceivePackets();
-			await ReceivePacketsTask;
+			ReceivePackets();
 		}
 
 		/// <summary>
@@ -79,10 +84,26 @@ namespace NExLib.Server
 			byte[] packetData = packet.ReturnData();
 
 			// Send the packet to all connected clients
-			foreach (IPEndPoint connectedClient in ConnectedClientsIdToIp.Values)
+			foreach (IPEndPoint connectedClient in ConnectedClientsIDToIP.Values)
 			{
 				UdpClient.Send(packetData, packetData.Length, connectedClient);
 			}
+		}
+
+		public void Listen(int packetType, PacketReceivedEventHandler method)
+		{
+			if (!PacketListeners.ContainsKey(packetType))
+			{
+				var packetListeners = new List<PacketReceivedEventHandler>
+				{
+					method
+				};
+
+				PacketListeners.Add(packetType, packetListeners);
+				return;
+			}
+
+			PacketListeners[packetType].Add(method);
 		}
 
 		/// <summary>
@@ -91,13 +112,13 @@ namespace NExLib.Server
 		/// <param name="packet">The packet to send.</param>
 		/// <param name="clientId">The client that the packet should be sent to.</param>
 		/// <returns></returns>
-		public void SendPacketTo(Packet packet, int clientId)
+		public void SendPacket(Packet packet, int clientId)
 		{
 			// Get data from the packet
 			byte[] packetData = packet.ReturnData();
 
 			// Send the packet to the specified client
-			if (ConnectedClientsIdToIp.TryGetValue(clientId, out IPEndPoint connectedClient))
+			if (ConnectedClientsIDToIP.TryGetValue(clientId, out IPEndPoint connectedClient))
 			{
 				UdpClient.Send(packetData, packetData.Length, connectedClient);
 			}
@@ -106,84 +127,109 @@ namespace NExLib.Server
 		/// <summary>
 		/// Receives up to MaxPacketsReceivedPerTick asynchronously.
 		/// </summary>
-		private async Task ReceivePackets()
+		private async void ReceivePackets()
 		{
-			if (UdpClient == null || !HasStarted || IsStopping)
+			if (UdpClient == null)
 			{
 				return;
 			}
 
-			Packet[] packets = new Packet[MaxPacketsReceivedPerTick];
 			for (int i = 0; i < MaxPacketsReceivedPerTick && UdpClient.Available > 0; i++)
 			{
-				// Extract data from the received packet
-				UdpReceiveResult udpReceiveResult = await UdpClient.ReceiveAsync();
-				IPEndPoint remoteEndPoint = udpReceiveResult.RemoteEndPoint;
-				byte[] packetData = udpReceiveResult.Buffer;
-
-				// Create new Packet object from the received packet data and invoke PacketReceived event
-				using (Packet packet = new Packet(packetData))
+				try
 				{
-					PacketReceived?.Invoke(packet, remoteEndPoint);
+					// Extract data from the received packet
+					UdpReceiveResult udpReceiveResult = await UdpClient.ReceiveAsync();
+					IPEndPoint remoteIPEndPoint = udpReceiveResult.RemoteEndPoint;
+					byte[] packetData = udpReceiveResult.Buffer;
+
+					// Create new packet object from the received packet data
+					using (Packet packet = new Packet(packetData))
+					{
+						// Check if packet contains header and data
+						if (packetData.Length <= 0)
+						{
+							LogHelper.LogMessage(LogHelper.LogLevel.Warning, $"Received an empty packet of type {packet.Type} (header and data missing).");
+						}
+						else if (packetData.Length < Packet.HeaderLength)
+						{
+							LogHelper.LogMessage(LogHelper.LogLevel.Warning, $"Received an empty packet of type {packet.Type} (header incomplete and data missing).");
+						}
+						else if (packetData.Length == Packet.HeaderLength)
+						{
+							LogHelper.LogMessage(LogHelper.LogLevel.Warning, $"Received an empty packet of type {packet.Type} (data missing).");
+						}
+
+						// Invoke packet received event
+						if (PacketReceived != null)
+						{
+							PacketReceived.Invoke(packet, remoteIPEndPoint);
+						}
+					}
+				}
+				catch (Exception e)
+				{
+					LogHelper.LogMessage(LogHelper.LogLevel.Error, $"Error occurred while trying to receive packet from server: {e}");
 				}
 			}
 		}
 
-		private void PacketReceivedHandler(Packet packet, IPEndPoint clientIPEndPoint)
+		private void OnPacketReceived(Packet packet, IPEndPoint IPEndPoint)
 		{
-			if (packet.Type == (int)DefaultPacketTypes.Connect)
+			foreach (PacketReceivedEventHandler packetReceivedEventHandler in PacketListeners[packet.Type])
 			{
-				ClientConnected(clientIPEndPoint);
-				return;
-			}
-
-			if (packet.Type == (int)DefaultPacketTypes.Disconnect)
-			{
-				ClientDisconnected(clientIPEndPoint);
-				return;
+				packetReceivedEventHandler.Invoke(packet, IPEndPoint);
 			}
 		}
 
-		private void ClientConnected(IPEndPoint clientIPEndPoint)
+		private void OnConnected(Packet packet, IPEndPoint IPEndPoint)
 		{
 			// Check if client is already connected
-			if (ConnectedClientsIdToIp.ContainsValue(clientIPEndPoint))
+			if (ConnectedClientsIDToIP.ContainsValue(IPEndPoint))
 			{
-				LogHelper.LogMessage(LogHelper.LogLevel.Warning, $"Client with IP {clientIPEndPoint} tried to connect, but is already connected!");
+				LogHelper.LogMessage(LogHelper.LogLevel.Warning, $"Client with IP {IPEndPoint} tried to connect, but is already connected!");
 				return;
 			}
 
 			// Accept the client's connection request
-			int clientId = ConnectedClientsIdToIp.Count;
-			ConnectedClientsIdToIp.Add(clientId, clientIPEndPoint);
-			ConnectedClientsIpToId.Add(clientIPEndPoint, clientId);
+			int clientId = ConnectedClientsIDToIP.Count;
+			ConnectedClientsIDToIP.Add(clientId, IPEndPoint);
+			ConnectedClientsIPToID.Add(IPEndPoint, clientId);
 
-			// Send a new packet back to the newly connected client
+			// Send a packet back to the client
 			using (Packet newPacket = new Packet((int)DefaultPacketTypes.Connect))
 			{
 				// Write the client ID to the packet
 				newPacket.Writer.Write(clientId);
 
-				SendPacketTo(newPacket, clientId);
+				SendPacket(newPacket, clientId);
 			}
 
-			LogHelper.LogMessage(LogHelper.LogLevel.Info, $"New client connected from {clientIPEndPoint}");
+			if (Connected != null)
+			{
+				Connected.Invoke(packet, IPEndPoint);
+			}
+			LogHelper.LogMessage(LogHelper.LogLevel.Info, $"New client connected from {IPEndPoint}");
 		}
 
-		private void ClientDisconnected(IPEndPoint clientIPEndPoint)
+		private void OnDisconnected(Packet packet, IPEndPoint IPEndPoint)
 		{
 			// Check if client is already disconnected
-			if (!ConnectedClientsIdToIp.ContainsValue(clientIPEndPoint))
+			if (!ConnectedClientsIDToIP.ContainsValue(IPEndPoint))
 			{
-				LogHelper.LogMessage(LogHelper.LogLevel.Warning, $"Client with IP {clientIPEndPoint} tried to disconnect, but is already disconnected!");
+				LogHelper.LogMessage(LogHelper.LogLevel.Warning, $"Client with IP {IPEndPoint} tried to disconnect, but has already disconnected!");
 				return;
 			}
 
 			// Disconnect the client
-			ConnectedClientsIdToIp.Remove(ConnectedClientsIpToId[clientIPEndPoint]);
-			ConnectedClientsIpToId.Remove(clientIPEndPoint);
+			ConnectedClientsIDToIP.Remove(ConnectedClientsIPToID[IPEndPoint]);
+			ConnectedClientsIPToID.Remove(IPEndPoint);
 
-			LogHelper.LogMessage(LogHelper.LogLevel.Info, $"Client {clientIPEndPoint} disconnected.");
+			if (Disconnected != null)
+			{
+				Disconnected.Invoke(packet, IPEndPoint);
+			}
+			LogHelper.LogMessage(LogHelper.LogLevel.Info, $"Client {IPEndPoint} disconnected.");
 		}
 	}
 }
